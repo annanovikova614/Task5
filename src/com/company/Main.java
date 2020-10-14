f7e407aaa93e22e9d0be13811be53328c0301423
annanovikova614 <72442702+annanovikova614@users.noreply.github.com>
2020-10-14 21:27:17 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,32 +4,42 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         int h = readVariebleH();
         int w = readVariebleW();
-        draw(h , w);
+        drawPicture(h, w);
     }
     private static int readVariebleH() {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите h: ");
         int h = in.nextInt();
-        return (int) h;
+        return h;
     }
-    private static int readVariebleW() {
+    private static int readVariebleW()
+    {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите w: ");
         int w = in.nextInt();
-        return (int) w;
+        return w;
     }
-    public static void draw(int h, int w) {
-        for(int i = h ; i > 0; i--) {
+
+    public static void drawPicture(int h, int w)
+    {
+        for(int i = h ; i > 0; i--)
+        {
             for(int j = 0; j < w; j++)
+            {
                 if(j / i % 2 == 0)
+                {
                     System.out.print("a");
+                }
                 else
+                {
                     System.out.print("b");
-            System.out.println();
+                }
+            }
+            System .out.println();
         }
     }
 }
-
