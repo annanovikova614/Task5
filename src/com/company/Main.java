4fdc5c2ed047fa50b5587c94d09632df6231c160
annanovikova614 <72442702+annanovikova614@users.noreply.github.com>
2020-10-24 11:47:48 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,30 +6,19 @@ public class Main {
 
     public static void main(String[] args)
     {
-        int h = readVariebleH();
-        int w = readVariebleW();
+        int h = readVariable("h");
+        int w = readVariable("w");
         drawPicture(h, w);
     }
-    private static int readVariebleH() {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Введите h: ");
-        int h = in.nextInt();
-        return h;
-    }
-    private static int readVariebleW()
-    {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Введите w: ");
-        int w = in.nextInt();
-        return w;
+    private static int readVariable(String variable) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Введите %s: ", variable);
+        return scanner.nextInt();
     }
 
-    public static void drawPicture(int h, int w)
-    {
-        for(int i = h ; i > 0; i--)
-        {
-            for(int j = 0; j < w; j++)
-            {
+    public static void drawPicture(int h, int w) {
+        for(int i = h ; i > 0; i--) {
+            for(int j = 0; j < w; j++) {
                 if(j / i % 2 == 0)
                 {
                     System.out.print("a");
